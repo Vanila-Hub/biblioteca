69aeb5a30d48cfa711a00e00beb4b8805a27c351
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-02-12 03:04:26 +0100

    ggg

--- biblioteca/src/biblioteca/GestorBBDD.java
+++ biblioteca/src/biblioteca/GestorBBDD.java
@@ -1,5 +1,33 @@
 package biblioteca;
 
 public class GestorBBDD extends Conector{
+	
+	public static void insertarLibro(Libro libro) {
+		
+	}
+	public static void eliminarLibro(int id) {
+		
+	}
+	public Libro getLibro(int id) {
+		return null;
+	}
+	public void modificarLibro(int id) {
 
+	}
+	
+	public static void verSocio(int id) {
+	        
+	}
+
+	public static void insertarSocio(Socio socio) {
+	        
+	}
+
+	public static void eliminarSocio(int id) {
+	        
+	 }
+
+	 public static void realizarPrestamo() {
+	        
+	 }
 }
