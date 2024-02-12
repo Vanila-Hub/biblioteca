d184e5dcac21aa47182491d25ce1710922e48b49
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-02-12 04:03:58 +0100

    gg

---
+++ biblioteca/src/biblioteca/FormulariosdeDatos.java
@@ -0,0 +1,15 @@
+package biblioteca;
+
+import java.util.Scanner;
+
+public class FormulariosdeDatos {
+	public static Libro  pedirDtaosLibro(Scanner scan) {
+		return null;
+	}
+	public static Libro  modificardatosLibro(Libro libro,Scanner scan) {
+		return null;
+	}
+	public static int pedirIdLibro(Scanner scan) {
+		return 0;
+	}
+}
