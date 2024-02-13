48f0981668c89ec9c39d219b7f578237a728b8ea
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-02-13 13:24:24 +0100

    j

--- biblioteca/src/biblioteca/GestorSocios.java
+++ biblioteca/src/biblioteca/GestorSocios.java
@@ -3,11 +3,17 @@ package biblioteca;
 import java.util.Scanner;
 
 public class GestorSocios {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		run(scan);
+
+	}
 	public static void run(Scanner scan) {
 		int opcion = 0;
 		do {
 			Menu m = new Menu();
 			m.mostrarMenuSocios();
+			opcion = Integer.parseInt(scan.nextLine());
 			switch (opcion) {
 			case Menu.INSERTAR_SOCIO:
 				System.out.println(Menu.INSERTAR_SOCIO);
@@ -16,10 +22,10 @@ public class GestorSocios {
 				System.out.println(Menu.ELIMINAR_SOCIO);
 				break;
 			case Menu.VER_SOCIO:
-				System.out.println(Menu.INSERTAR_SOCIO);
+				System.out.println(Menu.VER_SOCIO);
 				break;
 			case Menu.MODIFICAR_SOCIO:
-				System.out.println(Menu.VER_SOCIO);
+				System.out.println(Menu.MODIFICAR_SOCIO);
 				break;
 			default:
 				break;
